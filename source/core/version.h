f284f2d12656e11b734467251e40dcdda8d4e42c
Simon <simonbrown77@googlemail.com>
2023-06-20 22:37:11 +0100

    Update version.h

--- source/core/version.h
+++ source/core/version.h
@@ -44,7 +44,7 @@ const char *GetVersionString();
 
 #define VERSIONSTR "1.7pre"
 
-#define RAZEXR_VERSIONSTR "RazeXR 0.2.3"
+#define RAZEXR_VERSIONSTR "RazeXR 0.2.4"
 
 // The version as seen in the Windows resource
 #define RC_FILEVERSION 1,6,9999,0
